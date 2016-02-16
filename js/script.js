0c9a80cff53350b2963a26df345858881f714d99
Alex Lee <alexwlee.web@gmail.com>
2016-02-17 03:36:05 +1100

    added menu

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    $(".menu-button").on("click", function() {
+        $(".menu").toggleClass("menu-dropdown");
+        $(".menu-button").toggleClass("menu-white");
+    })
+
+    $('a[href^="#"]').on('click',function (e) {
+        e.preventDefault();
+
+        $(".menu").toggleClass("menu-dropdown");
+        $(".menu-button").toggleClass("menu-white");
+
+        var target = this.hash,
+        $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 700, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
+});
