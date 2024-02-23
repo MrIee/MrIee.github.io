ba7ad41da1ea85ef8a5b99d3d02eaba6d98111e2
mrlee <alexwlee.web@gmail.com>
2024-02-23 17:29:09 +1100

    Remove jQuery

--- js/script.js
+++ js/script.js
@@ -1,27 +1,3 @@
-// $(document).ready(function() {
-//     $(".menu__button").on("click", function() {
-//         $(".menu").show();
-//         $(".menu").toggleClass("tw-translate-y-full");
-//     });
-
-//     $('a[href^="#"]').on('click',function (e) {
-//         e.preventDefault();
-
-//         $(".menu").toggle();
-//         $(".menu").toggleClass("tw-translate-y-full");
-
-//         var target = this.hash,
-//         $target = $(target);
-//         var time = Math.abs(window.top.pageYOffset - $target.offset().top);
-
-//         $('html, body').stop().animate({
-//             'scrollTop': $target.offset().top
-//         }, time, 'swing', function () {
-//             window.location.hash = target;
-//         });
-//     });
-// });
-
 const setupMenuNav = () => {
   const menuBtnEl = document.getElementById('menuBtn');
   const menuEl = document.getElementById('menu');
