e8564e15ef38a4349933cbe5b80588a8cf2e4811
mrlee <alexwlee.web@gmail.com>
2024-02-23 17:28:43 +1100

    Refactor script from jQuery to vanilla JS

--- js/script.js
+++ js/script.js
@@ -1,23 +1,54 @@
-$(document).ready(function() {
-    $(".menu__button").on("click", function() {
-        $(".menu").show();
-        $(".menu").toggleClass("tw-translate-y-full");
-    });
+// $(document).ready(function() {
+//     $(".menu__button").on("click", function() {
+//         $(".menu").show();
+//         $(".menu").toggleClass("tw-translate-y-full");
+//     });
 
-    $('a[href^="#"]').on('click',function (e) {
-        e.preventDefault();
+//     $('a[href^="#"]').on('click',function (e) {
+//         e.preventDefault();
 
-        $(".menu").toggle();
-        $(".menu").toggleClass("tw-translate-y-full");
+//         $(".menu").toggle();
+//         $(".menu").toggleClass("tw-translate-y-full");
 
-        var target = this.hash,
-        $target = $(target);
-        var time = Math.abs(window.top.pageYOffset - $target.offset().top);
+//         var target = this.hash,
+//         $target = $(target);
+//         var time = Math.abs(window.top.pageYOffset - $target.offset().top);
 
-        $('html, body').stop().animate({
-            'scrollTop': $target.offset().top
-        }, time, 'swing', function () {
-            window.location.hash = target;
-        });
-    });
+//         $('html, body').stop().animate({
+//             'scrollTop': $target.offset().top
+//         }, time, 'swing', function () {
+//             window.location.hash = target;
+//         });
+//     });
+// });
+
+const setupMenuNav = () => {
+  const menuBtnEl = document.getElementById('menuBtn');
+  const menuEl = document.getElementById('menu');
+  const menuLinkEls = document.querySelectorAll('#menu a');
+
+  const onClickMenuBtn = () => {
+    menuEl.classList.remove('tw-hidden');
+    menuEl.classList.toggle('tw-translate-y-full');
+  };
+
+  const onClickMenuLink = () => {
+    const mediaQuery = window.matchMedia('(min-width: 1024px)');
+
+    if (mediaQuery.matches) {
+      return;
+    }
+
+    menuEl.classList.add('tw-hidden');
+    menuEl.classList.remove('tw-translate-y-full');
+  };
+
+  menuBtnEl.addEventListener('click', onClickMenuBtn);
+  menuLinkEls.forEach((menuLinkEl) => {
+    menuLinkEl.addEventListener('click', onClickMenuLink);
+  });
+};
+
+window.addEventListener("DOMContentLoaded", () => {
+  setupMenuNav();
 });
